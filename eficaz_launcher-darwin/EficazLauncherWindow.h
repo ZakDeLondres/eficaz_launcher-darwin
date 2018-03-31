a6b80263f20af54400df283c65aa09563143a808
Zakariyah Abdel-Illah <abs.zakariyah@gmail.com>
2018-03-31 22:08:10 +0100

    basic storyboard layout

---
+++ eficaz_launcher-darwin/EficazLauncherWindow.h
@@ -0,0 +1,13 @@
+//
+//  EficazLauncherWindow.h
+//  eficaz_launcher-darwin
+//
+//  Created by Zakariyah Abdel-Illah on 31/3/18.
+//  Copyright © 2018 AbsVFX. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface EficazLauncherWindow : NSWindow
+
+@end
---
+++ eficaz_launcher-darwin/EficazLauncherWindow.m
@@ -0,0 +1,13 @@
+//
+//  EficazLauncherWindow.m
+//  eficaz_launcher-darwin
+//
+//  Created by Zakariyah Abdel-Illah on 31/3/18.
+//  Copyright © 2018 AbsVFX. All rights reserved.
+//
+
+#import "EficazLauncherWindow.h"
+
+@implementation EficazLauncherWindow : NSWindow
+
+@end
